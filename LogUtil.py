07793627e680052ea10f8863f290d9105670af85
ArifArfi <arifarfi35@gmail.com>
2024-05-05 17:46:03 +0700

    push

---
+++ LogUtil.py
@@ -0,0 +1,41 @@
+"""
+Created on Mar 10, 2018
+
+@author: nopri
+"""
+
+import logging.config
+import logging
+import yaml
+import os
+
+
+class LogUtil:
+    """
+    class to handling logger
+    """
+
+    def __init__(self):
+        """
+        Constructor
+        """
+
+    @staticmethod
+    def setup_logging(
+            default_path='logging.yaml',
+            default_level=logging.INFO,
+            env_key='LOG_CFG'
+    ):
+        """
+        Setup logging configuration
+        """
+        path = default_path
+        value = os.getenv(env_key, None)
+        if value:
+            path = value
+        if os.path.exists(path):
+            with open(path, 'rt') as f:
+                config = yaml.safe_load(f.read())
+            logging.config.dictConfig(config)
+        else:
+            logging.basicConfig(level=default_level)
